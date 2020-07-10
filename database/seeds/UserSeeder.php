f26a82673c8468d56a8fbafbc9ac9c16dfa37305
Fahrur Razi <fahrurben@gmail.com>
2020-07-10 08:26:50 +0700

    User seeder add api token

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,10 +11,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $faker = \Faker\Factory::create();
+
         DB::table('users_table')->insert([
             'name' => 'Admin',
             'email' => 'admin@edir.com',
             'password' => \Illuminate\Support\Facades\Hash::make('admin'),
+            'api_token' => \Illuminate\Support\Facades\Hash::make($faker->password(4,4))
         ]);
     }
 }
